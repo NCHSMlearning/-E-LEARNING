e896b18b05127487ba815c29a623ddd199c47c9a
NCHSMlearning <mercycherotich2006@gmail.com>
2025-10-02 12:00:26 +0300

    Update App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the E-Learning Platform</h1>
+      <nav>
+        <a href="/">Home</a> |{" "}
+        <a href="/resources">E-Resource Center</a> |{" "}
+        <a href="/attendance">Attendance</a>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
