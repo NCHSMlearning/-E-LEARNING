1d7c19ae7be1f12027ccd1cc318a634acf2e7be2
NCHSMlearning <mercycherotich2006@gmail.com>
2025-10-05 11:17:24 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import ManageStudents from "./pages/ManageStudents";
 import AttendancePage from "./pages/AttendancePage";
 
 function App() {
   return (
-    <BrowserRouter basename="/-E-LEARNING">
-      <div style={{ padding: 20 }}>
+    <HashRouter>
+      <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
         <h1>E-Learning System</h1>
-        <nav style={{ marginBottom: 20 }}>
-          <Link to="/students" style={{ marginRight: 10 }}>Manage Students</Link>
-          <Link to="/attendance">Attendance</Link>
+        <nav>
+          <Link to="/">Students</Link> | <Link to="/attendance">Attendance</Link>
         </nav>
-
         <Routes>
           <Route path="/" element={<ManageStudents />} />
-          <Route path="/students" element={<ManageStudents />} />
           <Route path="/attendance" element={<AttendancePage />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
