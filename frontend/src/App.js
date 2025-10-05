34af41d53953cbb29b4dbabbacd7097383dce70d
NCHSMlearning <mercycherotich2006@gmail.com>
2025-10-05 11:12:39 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import ManageStudents from "./pages/ManageStudents";
 import AttendancePage from "./pages/AttendancePage";
 
 function App() {
   return (
-    <div>
-      <h1>E-Learning System</h1>
-      <ManageStudents />
-      <AttendancePage />
-    </div>
+    <BrowserRouter basename="/-E-LEARNING">
+      <div style={{ padding: 20 }}>
+        <h1>E-Learning System</h1>
+        <nav style={{ marginBottom: 20 }}>
+          <Link to="/students" style={{ marginRight: 10 }}>Manage Students</Link>
+          <Link to="/attendance">Attendance</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<ManageStudents />} />
+          <Route path="/students" element={<ManageStudents />} />
+          <Route path="/attendance" element={<AttendancePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
